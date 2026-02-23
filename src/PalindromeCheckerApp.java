e1af20e3d7cb6351c1853b3f8f2fe2f0f68a6e2f
ss3774-debug <ss3774@srmist.edu.in>
2026-02-23 15:25:48 +0530

    UC7: Deque-based optimized palindrome checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -92,4 +92,27 @@ import java.util.Stack;
             System.out.println("UC6 Result: '" + word + "' is NOT a palindrome.");
         }
     }
+    import java.util.ArrayDeque;
+import java.util.Deque;
+
+    public static void checkPalindromeDeque(String word) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : word.toCharArray()) {
+            deque.add(c);
+        }
+
+        boolean isPalindrome = true;
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("UC7 Result: '" + word + "' is a palindrome!");
+        } else {
+            System.out.println("UC7 Result: '" + word + "' is NOT a palindrome.");
+        }
+    }
 }
