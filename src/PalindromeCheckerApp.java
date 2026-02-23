fa74b72c2696155c088f6d336d0cf286c0c306c0
ss3774-debug <ss3774@srmist.edu.in>
2026-02-23 14:40:13 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -10,4 +10,15 @@ public class PalindromeCheckerApp {
         System.out.println("   Welcome to Palindrome Checker   ");
         System.out.println("====================================");
     }
+    // UC2: Hardcoded Palindrome
+    public static void checkHardcodedPalindrome() {
+        String word = "radar"; // hardcoded palindrome
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if (word.equals(reversed)) {
+            System.out.println("UC2 Result: '" + word + "' is a palindrome!");
+        } else {
+            System.out.println("UC2 Result: '" + word + "' is NOT a palindrome.");
+        }
+    }
 }
